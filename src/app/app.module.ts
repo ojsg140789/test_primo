36c92f9d499d266d0feaa0fd8b44266048264603
Omar Soto <omar.soto@invested.mx>
2022-08-03 14:37:43 -0500

    feat/Se agrega formulario reactivo para ingresar numero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouteReuseStrategy, RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -29,6 +29,7 @@ import { AppRoutingModule } from './app-routing.module';
     HomeModule,
     AuthModule,
     AppRoutingModule, // must be imported as the last module as it contains the fallback route
+    ReactiveFormsModule,
   ],
   declarations: [AppComponent],
   providers: [
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { QuoteService } from './quote.service';
 
@@ -12,19 +13,19 @@ export class HomeComponent implements OnInit {
   quote: string | undefined;
   isLoading = false;
 
-  constructor(private quoteService: QuoteService) {}
+  formData: FormGroup;
+  numbers: Array<any> = [];
 
-  ngOnInit() {
-    this.isLoading = true;
-    this.quoteService
-      .getRandomQuote({ category: 'dev' })
-      .pipe(
-        finalize(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe((quote: string) => {
-        this.quote = quote;
-      });
+  constructor(private fb: FormBuilder) {
+    this.formData = this.fb.group({
+      number: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  ngOnInit() {}
+
+  calculate() {
+    this.numbers = Array(5).map((x, i) => i);
+    console.log('this.numbers', this.numbers);
   }
 }
