a21f5c6bcc1bb507ddd9e765d25ccc401c1bcf7e
Omar Soto <omar.soto@invested.mx>
2022-08-03 19:07:14 -0500

    feat/Se modifica logica para calcular multiplos segun el requerimiento

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,11 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class HomeComponent implements OnInit {
   formData: FormGroup;
-  numbers: Array<any> = [];
+  multiplos: any = {
+    number: '',
+    color: '',
+    multiplos: [],
+  };
 
   constructor(private fb: FormBuilder, private store: AngularFirestore) {
     //Se crea estructura de formulario
@@ -21,22 +25,46 @@ export class HomeComponent implements OnInit {
   ngOnInit() {}
 
   calculate() {
-    //Se crea arreglo con numeros primo con ayuda del operador modulo
-    for (let index = 1; index <= this.formData.controls['number'].value; index++) {
-      if (index % 3 == 0 || index % 5 == 0 || index % 7 == 0) {
-        this.numbers.push(index);
-      }
+    this.multiplos = {
+      number: this.formData.controls['number'].value,
+      color: '',
+      multiplos: [],
+    };
+    let color = undefined;
+    //El número debe ser mayor a 0, si la funcion regresa verdadero es multiplo de 3 y se agrega al arreglo para indicar que tiene multiplo
+    if (this.formData.controls['number'].value > 0 && this.multiple(this.formData.controls['number'].value, 3)) {
+      color = 'verde';
+      this.multiplos.multiplos.push(3);
     }
+    //El número debe ser mayor a 0, si la funcion regresa verdadero es multiplo de 5 y se agrega al arreglo para indicar que tiene multiplo
+    if (this.formData.controls['number'].value > 0 && this.multiple(this.formData.controls['number'].value, 5)) {
+      color = !color ? 'rojo' : color;
+      this.multiplos.multiplos.push(5);
+    }
+    //El número debe ser mayor a 0, si la funcion regresa verdadero es multiplo de 7 y se agrega al arreglo para indicar que tiene multiplo
+    if (this.formData.controls['number'].value > 0 && this.multiple(this.formData.controls['number'].value, 7)) {
+      color = !color ? 'azul' : color;
+      this.multiplos.multiplos.push(7);
+    }
+    this.multiplos.color = color ? color : 'none';
+    console.log('this.multiplos', this.multiplos);
+
     //Cada que se realiza un calculo se agrega un documento a la base de datos
     this.store
       .collection('primosCollection')
       .add({
-        numero: this.formData.controls['number'].value,
-        primos: this.numbers,
+        primos: this.multiplos,
       })
       .then((res) => {})
       .catch((e) => {
         console.error(e);
       });
+  }
+
+  //Función que define si tiene multiplo por medio de dos parametros, el numero y el multipo a conocer
+  multiple(valor: number, multiple: number) {
+    let resto = valor % multiple;
+    if (resto == 0) return true;
+    else return false;
   }
 }
